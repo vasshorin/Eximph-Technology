af319e53f9ecd2b2fd76a0f0eda6c98748640ee4
vasshorin <)tSz%gBWz1>
2023-04-08 19:18:46 -0700

    Add ul to the product advantages

--- src/components/items.js
+++ src/components/items.js
@@ -16,49 +16,91 @@ export const products = [
       name: 'Wet-granulation',
       imageUrl: 'https://quarco.ru/assets/images/resources/45/3.jpg',
       description: 'Exmiph Pharmacy offers wet granulation mixers. These mixers are designed for mixing substances and powdered materials and their subsequent granulation for the production of medicines. The use of this equipment is relevant for companies in the pharmaceutical, chemical, food, ceramic industries, as well as for the production of products from metal powders.',
-      advantages: "Availability of a conical material container. Automatic system of raising the lid of the tank by means of a pneumatic cylinder. The presence of the jacket of the main tank, which allows to heat or cool the product during its processing (Optional). Frequency regulation of the mixer drive motors, which provides an optimum selection of mixer and chopper speed depending on the complexity of the product. The presence of an air filter that prevents the ingress of substance dust into the room. The presence of a central control system with a touch screen and a programmable logic controller, provides clear and easy control of the technological process. The design and compact size of the mixer reduce power consumption by up to 20% and save expensive space in the room. High-quality polishing (0.5 microns) on the inner surface of the mixer. Safety in operation: presence of locks on the main and exit hatches of the mixer, as well as automatic cut-off of drives power when any of these hatches is opened. Surfaces in contact with the product are made of high quality alloy steel AISI304;"
+      advantages: `Availability of a conical material container; 
+      Automatic system of raising the lid of the tank by means of a pneumatic cylinder; 
+      The presence of the jacket of the main tank, which allows to heat or cool the product during its processing (Optional); 
+      Frequency regulation of the mixer drive motors, which provides an optimum selection of mixer and chopper speed depending on the complexity of the product;
+      The presence of an air filter that prevents the ingress of substance dust into the room; 
+      The presence of a central control system with a touch screen and a programmable logic controller, provides clear and easy control of the technological process; 
+      The design and compact size of the mixer reduce power consumption by up to 20% and save expensive space in the room; 
+      High-quality polishing (0.5 microns) on the inner surface of the mixer. Safety in operation: presence of locks on the main and exit hatches of the mixer, as well as automatic cut-off of drives power when any of these hatches is opened. Surfaces in contact with the product are made of high quality alloy steel AISI304;`
     },
     {
       id: 3,
       name: 'Deduster',
       imageUrl: 'https://quarco.ru/assets/images/resources/49/1.jpg',
       description: 'The C&C200G series is the latest model with an advanced design.  They have the dual function of dedusting and moving pellets from bottom to top. The inlet of the dedusting machine can be adjusted to the output height of any model of tablet press, the output is combined with metal detector. In this way, a production line can be assembled to ensure the dedusting and lifting of tablets as well as metal detecting.',
-      advantages: 'The housing and hood of the machine have a block design. The height of the pellet outlet is easily adjustable. The machines inlet and outlet are rotatable (360 degrees), which makes the production process convenient and allows for maximum economy of working space. Quick-assembly and disassembly reliable design. High capacity, enables production of large batches of different types of tablets 3-35 mm in diameter. All parts in contact with pellets are made of stainless steel 316L. The design of the dedusting machine has no "dead zones" which makes its cleaning efficient and fully meets the requirements of GMP. The reliable drive ensures stable and low-noise operation. Distinctive features of the machine are polishing pellets on both sides, high dedusting efficiency, modern design and easy transportation.'
+      advantages: `The housing and hood of the machine have a block design;
+      The height of the pellet outlet is easily adjustable;
+      The machines inlet and outlet are rotatable (360 degrees), which makes the production process convenient and allows for maximum economy of working space;
+      Quick-assembly and disassembly reliable design. High capacity, enables production of large batches of different types of tablets 3-35 mm in diameter. All parts in contact with pellets are made of stainless steel 316L. The design of the dedusting machine has no "dead zones" which makes its cleaning efficient and fully meets the requirements of GMP. The reliable drive ensures stable and low-noise operation;
+      Distinctive features of the machine are polishing pellets on both sides, high dedusting efficiency, modern design and easy transportation.`
     },
     {
       id: 4,
-      name: 'Product 4',
+      name: 'Capsule polisher',
       imageUrl: 'https://quarco.ru/assets/images/resources/51/1.jpg',
-      description: '',
+      description: 'The C&C100C series capsule polishers combine the dual functions of capsule polishing and lifting. The input of the polisher can be connected to any model of capsule filling machine, and the output to the capsule sorter and metal detector. Thus, it is possible to build a production line providing polishing, lifting, capsule sorting as well as metal particle detection. The model is designed with a number of advanced technologies and advances in robotics.',
+      advantages: `Two functions: polishing and capsule lifting;
+      Compact design, freeing up additional space to accommodate equipment to be connected at the back;
+      The inlet and outlet of the machine are rotatable (360°), which makes the operation convenient and allows for maximum economy of working space;
+      The sorter automatically rejects underfilled capsules as well as empty, cracked, and split capsules. Quickly assembled and quickly disassembled reliable design;
+      All parts in contact with the product are made of 316L stainless steel or other material that complies with current pharmaceutical manufacturing standards'`,
     },
     {
       id: 5,
-      name: 'Product 5',
+      name: 'Deblistering machine',
       imageUrl: 'https://quarco.ru/assets/images/resources/52/1.jpg',
-      description: 'This is product 5',
+      description: 'The C&C400 series blister packer is designed to remove tablets and capsules from blister packs made of PVC or aluminum foil. It allows quick format change of loaded packages, automatic separation of packages and their contents.',
+      advantages: 'Reliable and safe machine operation. Suitable for processing a large number of package formats. Conforms to GMP.'
     },
     {
       id: 6,
-      name: 'Product 6',
+      name: 'Automatic decasplulator',
       imageUrl: 'https://quarco.ru/assets/images/resources/53/1.jpg',
-      description: 'This is product 6',
+      description: 'The automatic decapsulator C&C400 is designed for automatic separation of defective capsules into upper and lower halves, the drug content of which, after subsequent sifting, is suitable for secondary encapsulation. The equipment is equipped with safety and signaling devices. All main working areas of the machine are made of stainless steel in accordance with GMP requirements.',
+      advantages: 'The capsules coming through the transport chute from the sorter are pushed into the separating device by a forked gripper. As they move forward, the upper and lower capsule halves are individually clamped by the separating blades and continue to move along the guide formed by these blades until the halves are completely separated.'
     },
     {
       id: 7,
-      name: 'Product 7',
+      name: 'Rotary tablet press',
       imageUrl: 'https://quarco.ru/assets/images/resources/54/1.jpg',
-      description: 'This is product 7',
+      description: 'The C&C800 series rotary tablet press is designed for the production of tablets up to 25 mm in diameter. The C&C800 rotary tablet presses are mainly used in the pharmaceutical industry. They can also be used in the chemical, food, electronic and metallurgical industries.       This tablet press is designed to form a wide range of tablets from granular raw materials: flat-cylindrical, profiled, shaped, as well as with chamfers and risks on both sides.',
+      advantages: `Tablet  weight error ±1%. Maximum permissible weight difference between tablets: <0.3g ±5%, 0.3g ±3%.
+      Rotor, feeder and punches as well as granulate hopper and other parts in contact with the product are made of stainless steel AISI316L.
+      The capacity control (rotor speed) is in continuous mode. There is a function of keeping the speed in the set range. On the upper and lower punches installed oil seals, and additionally installed a special device against leakage of lubricant and dust protection. On the press it is possible to use a half set of the format tooling, and the positions where there are no punches and matrices are installed plugs.
+      Display of press force and output values on the control screen.`
     },
     {
       id: 8,
-      name: 'Product 8',
+      name: 'Single station laboratory press',
       imageUrl: 'https://quarco.ru/assets/images/resources/55/1.jpg',
-      description: 'This is product 8',
+      description: `The C&C600 series single station laboratory tablet press is a continuous duty machine with a completely new type of design.
+      It is designed for the pharmaceutical, chemical, electronic and food industries, as well as for laboratories with the need to make tablets from powdered or granular raw materials.
+      The presence of a protective enclosure that ensures the safety of the equipment and the operator. Thoughtful design, high level of robotization.
+      Providing additional space for operator control of the machine due to the design with four vertical supports;
+      Non-disconnecting drive increases smoothness of movement, reduces noise.`,
+      advantages: `Compactness and convenient operation;
+
+      High waste-free ratio of raw materials used for production;
+      Minimal difference in weight of tablets;
+      High pressure of pressing force;
+      Low noise level;
+      Possibility of continuous control of powder filling depth and thickness of tablets. It is able to produce round, shaped, profiled, ring-shaped tablets in different shapes.
+      It is the first choice equipment in the technological laboratories of pharmaceutical factories, pharmaceutical preparation departments of research institutes and hospitals. By its functional characteristics and indicators the machine fully meets the requirements of advanced international standards, in particular, GMP.
+      Original design;`,
     },
     {
       id: 9,
       name: 'Product 9',
       imageUrl: 'https://quarco.ru/assets/images/resources/43/1.jpg',
-      description: 'This is product 9',
+      description: `Eximph Techbologies offers CFM series hard gelatin capsule filling machines under the QUARCO brand name. This equipment is designed for filling solid gelatin capsules with powder and pellets. The machine is designed for use in medium to large scale pharmaceutical production and meets all GMP requirements.`,
+      advantages: `The changes made to the machine design and the use of gearboxes and drives from leading world manufacturers have allowed us to achieve a reduction in vibration and noise, ensure virtually smooth operation of the main components, and increase the time interval between preventive maintenance of these elements.
+      Compact dimensions;
+      Quick changeover from one format to another;
+      Quick changeover from powder filling to pellet filling;
+      Fast cleaning thanks to minimum number of accessories;
+      Achieve high dosing accuracy with the volumetric dosing method;
+      Touch screen (Touch Screen);`
     },
   ];
--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -11,24 +11,23 @@ const ProductDetails = () => {
   if (!product) {
     return <h2>Product not found</h2>;
   }
+  const advantagesList = product.advantages.split("\n").map((advantage) => {
+    if (advantage) {
+      return <li>{advantage.trim()}</li>;
+    }
+  });
 
   return (
     <div className="product-details">
       <div className="product-image">
-        {/* <Carousel showArrows={true}>
-          {product.images.map((image, index) => (
-            <div key={index}>
-              <img src={image} alt={product.name} />
-            </div>
-          ))}
-        </Carousel> */}
         <img className='product-image' src={product.imageUrl} alt={product.name} />
       </div>
       <div className="">
         <h2 className="product-title">{product.name}</h2>
         <p className="products-">{product.description}</p>
         <h2 className="product-title">Advantages</h2>
-        <p className="advantages">{product.advantages}</p>
+        {/* <p className="advantages">{product.advantages}</p> */}
+        <ul>{advantagesList}</ul>
       </div>
     </div>
   );
