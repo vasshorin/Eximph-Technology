3f659728605610b2c4fa3dea5e1fb242fe7a92c3
Vasily <vshorin@my.bcit.ca>
2023-06-25 16:29:58 -0700

    Change navbar design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 shadow-lg">
+    <nav className="bg-gray-800 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -67,53 +67,52 @@ const Navbar = () => {
               <div className="flex space-x-4">
                 <Link
                   to="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-white-700 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200"
                   aria-current="page"
                 >
                   Home
                 </Link>
-
                 <Link
                   to="/about"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-white-700 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200"
                 >
                   About
                 </Link>
-
                 <Link
                   to="/products"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-white-700 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200"
                 >
                   Products
                 </Link>
               </div>
             </div>
           </div>
+
         </div>
       </div>
-              
-      
+
+
       {isMenuOpen && (
         <div className="sm:hidden" id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1">
             <Link
               to="/"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-white hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               aria-current="page"
             >
               Home
             </Link>
-          
+
             <Link
               to="/about"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-white hover:bg-white-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               About
             </Link>
 
             <Link
               to="/products"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              className="text-white hover:bg-white-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               Products
             </Link>
