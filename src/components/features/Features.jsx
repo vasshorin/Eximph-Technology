4bdf0865833190715f897e7ccc2ab9b4654f8d27
Vasily <vshorin@my.bcit.ca>
2023-06-25 14:58:29 -0700

    Add features

---
+++ src/components/features/Features.jsx
@@ -0,0 +1,245 @@
+export const Features = () => {
+    return (
+        <>
+        <section class="bg-white dark:bg-gray-900">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+      <div class="max-w-screen-md mb-8 lg:mb-16">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Service we provide</h2>
+          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Eximph we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+      </div>
+      <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Delivery</h3>
+              <p class="text-gray-500 dark:text-gray-400">Our team will help you with the delivery of your equipment to the destination of your choice.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Factory acceptance tests</h3>
+              <p class="text-gray-500 dark:text-gray-400">When the equipment is ready, we will perform the factory acceptance tests to ensure that everything is working properly, and up to your standards.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Commissioning</h3>
+              <p class="text-gray-500 dark:text-gray-400">Once the equipment is delivered, our personel will help you with the commissioning of the equipment at your factory.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd"></path></svg>
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Personal training</h3>
+              <p class="text-gray-500 dark:text-gray-400">To ensure that your team is ready to operate the equipment, we will provide training for your personel.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Logistics</h3>
+              <p class="text-gray-500 dark:text-gray-400">We will take care of the logistics of the equipment from the manufacturers to your factory.</p>
+          </div>
+          <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Documentation</h3>
+              <p class="text-gray-500 dark:text-gray-400">We will provide all of the designing and technical documentation for the equipment to help you choose the right products that fit your needs.</p>
+          </div>
+      </div>
+  </div>
+</section></>
+    //   <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    //     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    //       <div>
+    //         <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+    //           Brand new
+    //         </p>
+    //       </div>
+    //       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+    //         <span className="relative inline-block">
+    //           <svg
+    //             viewBox="0 0 52 24"
+    //             fill="currentColor"
+    //             className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+    //           >
+    //             <defs>
+    //               <pattern
+    //                 id="27df4f81-c854-45de-942a-fe90f7a300f9"
+    //                 x="0"
+    //                 y="0"
+    //                 width=".135"
+    //                 height=".30"
+    //               >
+    //                 <circle cx="1" cy="1" r=".7" />
+    //               </pattern>
+    //             </defs>
+    //             <rect
+    //               fill="url(#27df4f81-c854-45de-942a-fe90f7a300f9)"
+    //               width="52"
+    //               height="24"
+    //             />
+    //           </svg>
+    //           <span className="relative">The</span>
+    //         </span>{' '}
+    //         quick, brown fox jumps over a lazy dog
+    //       </h2>
+    //       <p className="text-base text-gray-700 md:text-lg">
+    //         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    //         accusantium doloremque rem aperiam, eaque ipsa quae.
+    //       </p>
+    //     </div>
+    //     <div className="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
+    //       <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+    //         <div className="mr-4">
+    //           <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+    //             <svg
+    //               className="w-10 h-10 text-deep-purple-accent-400"
+    //               stroke="currentColor"
+    //               viewBox="0 0 52 52"
+    //             >
+    //               <polygon
+    //                 strokeWidth="3"
+    //                 strokeLinecap="round"
+    //                 strokeLinejoin="round"
+    //                 fill="none"
+    //                 points="29 13 14 29 25 29 23 39 38 23 27 23"
+    //               />
+    //             </svg>
+    //           </div>
+    //         </div>
+    //         <div>
+    //           <h6 className="mb-3 text-xl font-bold leading-5">The deep ocean</h6>
+    //           <p className="mb-3 text-sm text-gray-900">
+    //             A flower in my garden, a mystery in my panties. Heart attack never
+    //             stopped old Big Bear. I didn't even know we were calling him Big
+    //             Bear. We never had the chance to.
+    //           </p>
+    //           <a
+    //             href="/"
+    //             aria-label=""
+    //             className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+    //           >
+    //             Learn more
+    //           </a>
+    //         </div>
+    //       </div>
+    //       <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+    //         <div className="mr-4">
+    //           <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+    //             <svg
+    //               className="w-10 h-10 text-deep-purple-accent-400"
+    //               stroke="currentColor"
+    //               viewBox="0 0 52 52"
+    //             >
+    //               <polygon
+    //                 strokeWidth="3"
+    //                 strokeLinecap="round"
+    //                 strokeLinejoin="round"
+    //                 fill="none"
+    //                 points="29 13 14 29 25 29 23 39 38 23 27 23"
+    //               />
+    //             </svg>
+    //           </div>
+    //         </div>
+    //         <div>
+    //           <h6 className="mb-3 text-xl font-bold leading-5">
+    //             When has justice
+    //           </h6>
+    //           <p className="mb-3 text-sm text-gray-900">
+    //             Rough pomfret lemon shark plownose chimaera southern sandfish
+    //             kokanee northern sea robin Antarctic cod. Yellow-and-black
+    //             triplefin gulper South American Lungfish mahi-mahi, butterflyfish
+    //             glass catfish soapfish ling gray mullet!
+    //           </p>
+    //           <a
+    //             href="/"
+    //             aria-label=""
+    //             className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+    //           >
+    //             Learn more
+    //           </a>
+    //         </div>
+    //       </div>
+    //       <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+    //         <div className="mr-4">
+    //           <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+    //             <svg
+    //               className="w-10 h-10 text-deep-purple-accent-400"
+    //               stroke="currentColor"
+    //               viewBox="0 0 52 52"
+    //             >
+    //               <polygon
+    //                 strokeWidth="3"
+    //                 strokeLinecap="round"
+    //                 strokeLinejoin="round"
+    //                 fill="none"
+    //                 points="29 13 14 29 25 29 23 39 38 23 27 23"
+    //               />
+    //             </svg>
+    //           </div>
+    //         </div>
+    //         <div>
+    //           <h6 className="mb-3 text-xl font-bold leading-5">
+    //             Organically grow
+    //           </h6>
+    //           <p className="mb-3 text-sm text-gray-900">
+    //             A slice of heaven. O for awesome, this chocka full cuzzie is as
+    //             rip-off as a cracker. Meanwhile, in behind the bicycle shed,
+    //             Hercules Morse, as big as a horse and Mrs Falani were up to no
+    //             good with a bunch of crook pikelets.
+    //           </p>
+    //           <a
+    //             href="/"
+    //             aria-label=""
+    //             className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+    //           >
+    //             Learn more
+    //           </a>
+    //         </div>
+    //       </div>
+    //       <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+    //         <div className="mr-4">
+    //           <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+    //             <svg
+    //               className="w-10 h-10 text-deep-purple-accent-400"
+    //               stroke="currentColor"
+    //               viewBox="0 0 52 52"
+    //             >
+    //               <polygon
+    //                 strokeWidth="3"
+    //                 strokeLinecap="round"
+    //                 strokeLinejoin="round"
+    //                 fill="none"
+    //                 points="29 13 14 29 25 29 23 39 38 23 27 23"
+    //               />
+    //             </svg>
+    //           </div>
+    //         </div>
+    //         <div>
+    //           <h6 className="mb-3 text-xl font-bold leading-5">
+    //             A slice of heaven
+    //           </h6>
+    //           <p className="mb-3 text-sm text-gray-900">
+    //             Disrupt inspire and think tank, social entrepreneur but
+    //             preliminary thinking think tank compelling. Inspiring, invest
+    //             synergy capacity building, white paper; silo, unprecedented
+    //             challenge B-corp problem-solvers.
+    //           </p>
+    //           <a
+    //             href="/"
+    //             aria-label=""
+    //             className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+    //           >
+    //             Learn more
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    );
+  };
