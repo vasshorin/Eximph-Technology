89a654c7e624670da719d1eabebdd095b6ebd556
Vasily <vshorin@my.bcit.ca>
2023-06-29 14:32:28 -0700

    Fix naming

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'tailwindcss/tailwind.css';
 import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
