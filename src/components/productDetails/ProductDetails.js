3b801416c13c3aeec0b0736ad492da7c7a19a410
Vasily <vshorin@my.bcit.ca>
2023-07-03 10:12:09 -0700

    Update colors

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -88,7 +88,7 @@ const ProductDetails = () => {
             ) : null}
             
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4"
+              className="bg-gray-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4"
               onClick={handleQuoteRequest}
             >
               Request a quote
